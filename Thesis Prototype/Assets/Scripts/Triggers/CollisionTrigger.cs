7e12ca18ce10c35673e79bf621af342622acbe1c
Yves <yves010449@gmail.com>
2023-01-24 21:55:06 +0800

    0.1.5

---
+++ Thesis Prototype/Assets/Scripts/Triggers/CollisionTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+using UnityEngine.Events;
+
+public class CollisionTrigger : MonoBehaviour {
+
+    public UnityEvent OnTrigger;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        OnTrigger?.Invoke();
+        gameObject.SetActive(false);
+        
+    }
+}
